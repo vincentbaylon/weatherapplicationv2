ea5abe1f23b48c4a25c3abc1969c0f25dbde8a40
vincentbaylon <vincentbaylon@gmail.com>
2021-07-12 15:25:36 -0700

    Fixed horizontal bar

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function fetchWeather(city) {
+// Fetch
+function fetchSingleCity(city) {
     if (city.includes(' ')) {
         let cityArray = city.split(' ')
         city = cityArray.join('_')
@@ -6,7 +7,26 @@ function fetchWeather(city) {
     console.log(`CITY: ${city}`)
     fetch(`https://goweather.herokuapp.com/weather/${city}`)
     .then(res => res.json())
+    .then(renderWeather)
+
+    // .then(json => renderWeather())
+}
+
+fetchSingleCity('San Francisco')
+
+function fetchWeather() {
+    fetch(`https://goweather.herokuapp.com/weather/`)
+    .then(res => res.json())
     .then(console.log)
 }
 
-fetchWeather('San Francisco')
+// Render
+function renderWeather(city) {
+    let div = document.querySelector('#about')
+
+    let li = document.createElement('li')
+
+    li.textContent = city.description
+    
+    div.append(li)
+}
